c448476a5c344f2f897e72c493533446e3559d54
Misael Duarte <msl.snzn@gmail.com>
2021-09-28 19:48:16 +0100

    Agregada ruta de ejemplo al ejemplo simple de Express

--- express/index.js
+++ express/index.js
@@ -7,7 +7,13 @@ const app = express()
 
 // Definición de ruta donde express va a escuchar
 app.get("/", (req, res, next) => {
-  res.send(`Holiwi desde servidor`)
+  console.log(`Me peticionaron desde ${req.originalUrl}`)
+  res.send(`Choy un Express server`)
+})
+
+app.get('/saluda', (req, res, next) => {
+  console.log(`Me peticionaron desde ${req.originalUrl}`)
+  res.send(`Holiwi querido usador`)
 })
 
 // Creación del servidor
@@ -16,4 +22,6 @@ const server = http.Server(app)
 // Arrancar el servidor
 const PORT = 8085
 
-server.listen(PORT, () => console.log(`Express server running on port ${PORT}`))
+server.listen(PORT, () => {
+  console.log(`Express server running on port ${PORT}`)
+})
